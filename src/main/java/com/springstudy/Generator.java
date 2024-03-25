6beac35dfe95749706b3113ff5d92515a206ee3f
xiewenyan <1441950483@qq.com>
2024-03-25 11:44:16 +0800

    自动生成代码

--- src/main/java/com/springstudy/Generator.java
+++ src/main/java/com/springstudy/Generator.java
@@ -46,7 +46,7 @@ public class Generator {
         autoGenerator.setPackageInfo(packageConfig);
         // 策略配置
         StrategyConfig strategyConfig = new StrategyConfig();
-        strategyConfig.setInclude("expert_service");
+        strategyConfig.setInclude("sys_user_bound");
 
         strategyConfig.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
 
