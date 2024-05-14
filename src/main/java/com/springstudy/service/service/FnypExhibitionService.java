8f088a36fbc230a568be0dc56b1183138231fe0d
xiewenyan <1441950483@qq.com>
2024-05-14 10:38:17 +0800

    自动生成代码

---
+++ src/main/java/com/springstudy/service/service/FnypExhibitionService.java
@@ -0,0 +1,15 @@
+package com.springstudy.service.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.springstudy.service.entity.FnypExhibition;
+
+/**
+ * 展会表(FnypExhibition)表服务接口
+ *
+ * @author makejava
+ * @since 2024-05-13 11:10:12
+ */
+public interface FnypExhibitionService extends IService<FnypExhibition> {
+
+}
+
